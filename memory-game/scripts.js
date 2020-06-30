f90ba286bedbf8816467f24981b2014f1f3331ae
Andres Forero <afforeroc@unal.edu.co>
2020-06-30 17:08:20 -0500

    Memory Card Game - JavaScript Tutorial - Part 7

--- memory-game/scripts.js
+++ memory-game/scripts.js
@@ -1,19 +1,15 @@
-const cards = document.querySelectorAll('.memory-card');
-// Make a list of all memory-cards elements and store in a constant object
+const cards = document.querySelectorAll('.memory-card'); // Constant list of all memory-cards.
+
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard(){
-    this.classList.toggle('flip'); // Modify the name class of div in the html file. In this way: 'memory-card => 'memory-card flip'
-    if (!hasFlippedCard) {
-        // First click
+    this.classList.add('flip'); // Do this in this way: 'memory-card => 'memory-card flip'.
+    if (!hasFlippedCard) { // First click.
         hasFlippedCard = true;
-        firstCard = this;''
-
-        console.log(hasFlippedCard, firstCard);
-    } else {
-        // Second click
+        firstCard = this;
+    } else { // Second click.
         hasFlippedCard = false;
         secondCard = this;
         console.log({firstCard, secondCard});
@@ -23,7 +19,6 @@ function flipCard(){
     }
 }
 
-// Link each memory-card a listen a event
-// When is clicked one memory card, this will flip
-cards.forEach(card => card.addEventListener('click', flipCard));
+
+cards.forEach(card => card.addEventListener('click', flipCard)); // Link each memory-card a listen a event.
 
